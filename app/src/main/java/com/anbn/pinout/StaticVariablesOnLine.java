ad4cafe763407797a3726f1fe9db398ef6a62164
Aleksandr Butakov <anbngm@gmail.com>
2022-12-01 10:15:28 +0400

    Added onLine manuals

--- app/src/main/java/com/anbn/pinout/StaticVariablesOnLine.java
+++ app/src/main/java/com/anbn/pinout/StaticVariablesOnLine.java
@@ -7,7 +7,10 @@ public class StaticVariablesOnLine {
             "Выберите оборудование...",
             "Cisco Systems",
             "Оборудование DX500",
-            "ECI Telecom"};
+            "ECI Telecom",
+            "Радиостанции",
+            "Телефоны, ППСЦ",
+            "Zyxel"};
 
     //  Оборудование Cisco Systems
     public static final String[] CARD1 = {
@@ -48,6 +51,40 @@ public class StaticVariablesOnLine {
                     "https://drive.google.com/file/d/1ez8jh1JgwGaJaQ-MTRWPEySav6HdLXhR/view?usp=sharing"
             };
 
+    //  Радиостанции
+    public static final String[] CARD4 = {
+            "Выберите оборудование...",
+            "РС-46МЦ",      // 4, 1
+            "РВС-1"};       // 4, 2
+    // массив с адресами файлов
+    public static final String[] CARD4URL =
+            {
+                    "",
+                    "https://drive.google.com/file/d/1G0gPIwrxZv0Lykw0r5ydN26sFsLtz5zJ/view?usp=sharing",
+                    "https://drive.google.com/file/d/1ob5KlS4xeu1OTu3zylQkBFuv25sESQy2/view?usp=sharing"
+            };
+
+    //  Телефоны, ППСЦ
+    public static final String[] CARD5 = {
+            "Выберите оборудование...",
+            "Телефоны, ППСЦ"};      // 5, 1
+    // массив с адресами файлов
+    public static final String[] CARD5URL =
+            {
+                    "",
+                    "https://drive.google.com/file/d/1Tfi_Fj0U8px5N_RYvFZ_X8KSmrCs5iMY/view?usp=sharing"
+            };
+
+    //  Zyxel
+    public static final String[] CARD6 = {
+            "Выберите оборудование...",
+            "Zyxel P-793H"};      // 6, 1
+    // массив с адресами файлов
+    public static final String[] CARD6URL =
+            {
+                    "",
+                    "https://drive.google.com/file/d/1vzzRDGDY9P9TAcC1YxWHfSoXPWQ0IBeG/view?usp=sharing"
+            };
 
 
 }
