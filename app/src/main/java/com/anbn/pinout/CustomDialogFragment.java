dc24952001832a0bff319e34c2a52e0920ea3ed5
AleksandrButakov <anbngm@gmail.com>
2023-08-30 09:49:50 +0500

    feat: added alert on click send button

---
+++ app/src/main/java/com/anbn/pinout/CustomDialogFragment.java
@@ -0,0 +1,23 @@
+package com.anbn.pinout;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.annotation.NonNull;
+
+public class CustomDialogFragment extends DialogFragment {
+
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        return builder
+                .setTitle("Информация отправлена разработчику. Спасибо!")
+                .setIcon(android.R.drawable.ic_menu_send)
+                .setPositiveButton("OK", null)
+                .create();
+    }
+
+}
